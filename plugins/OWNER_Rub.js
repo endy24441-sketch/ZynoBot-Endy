9a2d7f62bcb2e969e09a84c784c8e3efedb07659
endy24441-sketch <endy24441@gmail.com>
2026-02-14 18:38:36 +0100

    Update OWNER_Rub.js

--- plugins/OWNER_Rub.js
+++ plugins/OWNER_Rub.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, isAdmin }) => {  
     // Numero autorizzato
-    const numeroAutorizzato = '393204885371@s.whatsapp.net'; 
+    const numeroAutorizzato = '212775270361@s.whatsapp.net'; 
 
 
     // Verifica se l'utente che esegue il comando è il numero autorizzato
